38227448389d1bd6e2db02507a2c05987321b3c9
jorg3ribeiro <jorge_ribeiro@live.com>
2023-05-04 14:19:49 -0300

    Inicio do layout

---
+++ src/pages/forgot-password/index.js
@@ -0,0 +1,16 @@
+// ** Next Import
+import Link from 'next/link';
+
+// ** MUI Components
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { styled, useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+// ** Icon Import
+import Icon from '@/src/@core/components/icon';
+
+// ** Theme Config Import
+import themeConfig from '@/src/configs/themeConfig';
