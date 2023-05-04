b645c774a6cc2603a08f1b33142cfdcda15dcc65
JefersonMuniiiz <fersonmuniz34@gmail.com>
2023-05-04 16:18:14 -0300

    Criaçao pagina esqueceu-senha

--- src/pages/forgot-password/index.js
+++ src/pages/forgot-password/index.js
@@ -1,16 +1,162 @@
 // ** Next Import
-import Link from 'next/link';
+import Link from 'next/link'
 
 // ** MUI Components
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { styled, useTheme } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { styled, useTheme } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
 
-// ** Icon Import
-import Icon from '@/src/@core/components/icon';
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
 
-// ** Theme Config Import
-import themeConfig from '@/src/configs/themeConfig';
+// ** Configs
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Layout Import
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+// ** Hooks
+import { useSettings } from 'src/@core/hooks/useSettings'
+
+// ** Demo Imports
+import FooterIllustrations from 'src/views/pages/misc/FooterIllustrations';
+
+// Styled Components
+const ForgotPasswordIllustrationWrapper = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(20),
+  paddingRight: '0 !important',
+  [theme.breakpoints.down('lg')]: {
+    padding: theme.spacing(10)
+  }
+}))
+
+const ForgotPasswordIllustration = styled('img')(({ theme }) => ({
+  maxWidth: '48rem',
+  [theme.breakpoints.down('xl')]: {
+    maxWidth: '38rem'
+  },
+  [theme.breakpoints.down('lg')]: {
+    maxWidth: '30rem'
+  }
+}))
+
+const RightWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  [theme.breakpoints.up('md')]: {
+    maxWidth: 400
+  },
+  [theme.breakpoints.up('lg')]: {
+    maxWidth: 450
+  }
+}))
+
+const BoxWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  [theme.breakpoints.down('md')]: {
+    maxWidth: 400
+  }
+}))
+
+const TypographyStyled = styled(Typography)(({ theme }) => ({
+  fontWeight: 600,
+  letterSpacing: '0.18px',
+  marginBottom: theme.spacing(1.5),
+  [theme.breakpoints.down('md')]: { marginTop: theme.spacing(8) }
+}))
+
+const LinkStyled = styled(Link)(({ theme }) => ({
+  display: 'flex',
+  '& svg': { mr: 1.5 },
+  alignItems: 'center',
+  textDecoration: 'none',
+  justifyContent: 'center',
+  color: theme.palette.primary.main
+}))
+
+const ForgotPassword = () => {
+  // ** Hooks
+  const theme = useTheme()
+  const { settings } = useSettings()
+
+  // ** Vars
+  const { skin } = settings
+  const hidden = useMediaQuery(theme.breakpoints.down('md'))
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  const imageSource =
+    skin === 'bordered' ? 'gestaoProjeto-bordered' : 'gestaoProjeto'
+
+  return (
+    <Box className='content-right'>
+      {!hidden ? (
+        <Box sx={{ flex: 1, display: 'flex', position: 'relative', alignItems: 'center', justifyContent: 'center' }}>
+          <ForgotPasswordIllustrationWrapper>
+            <ForgotPasswordIllustration
+              alt='forgot-password-illustration'
+              src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
+            />
+          </ForgotPasswordIllustrationWrapper>
+          <FooterIllustrations />
+        </Box>
+      ) : null}
+      <RightWrapper sx={skin === 'bordered' && !hidden ? { borderLeft: `1px solid ${theme.palette.divider}` } : {}}>
+        <Box
+          sx={{
+            p: 7,
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'background.paper'
+          }}
+        >
+          <BoxWrapper>
+            <Box
+              sx={{
+                top: 30,
+                left: 40,
+                display: 'flex',
+                position: 'absolute',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <img alt='error-illustration' src='/images/logo.png' style={{ width: 44, height: 47 }} />
+              <Typography variant='h6' sx={{ ml: 2, lineHeight: 1, fontWeight: 700, fontSize: '1.5rem !important' }}>
+                {themeConfig.templateName}
+              </Typography>
+            </Box>
+            <Box sx={{ mb: 6 }}>
+              <TypographyStyled variant='h5'>Esqueceu sua Senha? 🔒</TypographyStyled>
+              <Typography variant='body2'>
+              Um link de redefinição de senha será enviado para o seu endereço de e-mail. 
+              </Typography>
+            </Box>
+            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+              <TextField autoFocus type='email' label='Email' sx={{ display: 'flex', mb: 4 }} />
+              <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 5.25 }}>
+                Enviar
+              </Button>
+              <Typography sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <LinkStyled href='/login'>
+                  <Icon icon='mdi:chevron-left' fontSize='2rem' />
+                  <span>voltar à página de login</span>
+                </LinkStyled>
+              </Typography>
+            </form>
+          </BoxWrapper>
+        </Box>
+      </RightWrapper>
+    </Box>
+  )
+}
+ForgotPassword.guestGuard = true
+ForgotPassword.getLayout = page => <BlankLayout>{page}</BlankLayout>
+
+export default ForgotPassword
